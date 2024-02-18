1a71b15e3662a167109be54673c5d4f8bd280064
athena272 <guilhermera272@gmail.com>
2024-02-18 12:02:16 -0300

    Update PostModel.jsx

--- src/component/PostModel/PostModel.jsx
+++ src/component/PostModel/PostModel.jsx
@@ -1,3 +1,27 @@
-export default function PostModel() {
-    
+import styles from './PostModel.module.scss'
+import PropTypes from 'prop-types'
+
+PostModel.propTypes = {
+    coverPhoto: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+}
+
+export default function PostModel({ coverPhoto, title, children }) {
+    return (
+        <article className={styles.postModelContainer}>
+            <div
+                className={styles.coverPhoto}
+                style={{ backgroundImage: `url(${coverPhoto})` }}
+            ></div>
+
+            <h2 className={styles.title}>
+                {title}
+            </h2>
+
+            <div className={styles.postConteudoContainer}>
+                {children}
+            </div>
+        </article>
+    )
 }
