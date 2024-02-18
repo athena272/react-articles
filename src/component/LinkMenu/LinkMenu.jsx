25e55a26815fc8f3b99edf922ed86d0ebc7922d1
athena272 <guilhermera272@gmail.com>
2024-02-18 11:40:51 -0300

    Create LinkMenu.jsx

---
+++ src/component/LinkMenu/LinkMenu.jsx
@@ -0,0 +1,22 @@
+import styles from 'LinkMenu.module.scss'
+import PropTypes from "prop-types"
+import { Link, useLocation } from 'react-router-dom';
+
+LinkMenu.propTypes = {
+    to: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+}
+
+export default function LinkMenu({ children, to }) {
+    const location = useLocation();
+    console.log("🚀 ~ LinkMenu ~ location:", location)
+
+    return (
+        <Link className={`
+            ${styles.link}
+            ${location.pathname === to ? styles.linkVisited : ""}
+        `} to={to}>
+            {children}
+        </Link>
+    )
+}
