203232e5106cdb0207f9713951807cfadfc5bf95
athena272 <guilhermera272@gmail.com>
2024-02-17 23:55:00 -0300

    Update MainButton.jsx

--- src/component/MainButton/MainButton.jsx
+++ src/component/MainButton/MainButton.jsx
@@ -1,4 +1,11 @@
 import styles from './MainButton.module.scss'
+import PropTypes from "prop-types"
+
+MainButton.propTypes = {
+    size: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+}
+
 
 export default function MainButton({ size, children }) {
     return (
