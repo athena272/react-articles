c280a1b9d9f357b29f465b0f9d424807cfbc8839
athena272 <guilhermera272@gmail.com>
2024-02-18 12:58:01 -0300

    Create Post.jsx

---
+++ src/pages/Post/Post.jsx
@@ -0,0 +1,7 @@
+import styles from './Post.module.scss'
+
+import { Route, Routes, useParams } from 'react-router-dom'
+import posts from 'assets/posts'
+import PostModel from '../../component/PostModel'
+import ReactMarkdown from 'react-markdown'
+import NotFou
