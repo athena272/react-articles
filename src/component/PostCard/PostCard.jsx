3a572cc0908f5dc5e46d5bbbea16b11d120d5355
athena272 <guilhermera272@gmail.com>
2024-02-17 23:58:57 -0300

    Create PostCard.jsx

---
+++ src/component/PostCard/PostCard.jsx
@@ -0,0 +1,11 @@
+import styles from './PostCard.module.scss'
+import PropTypes from 'prop-types'
+
+PostCard.propTypes = {
+    post: PropTypes.object.isRequired,
+}
+
+
+export default function PostCard({ post }) {
+
+}
