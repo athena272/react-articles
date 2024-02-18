a35744489a33a9bcc8c8b7b3a7b01ccc0918050f
athena272 <guilhermera272@gmail.com>
2024-02-18 15:53:57 -0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,15 @@
 import styles from './Home.module.scss'
-import posts from '../../../public/assets/json/posts.json'
+import posts from 'assets/json/posts.json'
+import PostCard from '../../component/PostCard/PostCard'
 
 export default function Home() {
     return (
-        
+        <ul className={styles.posts}>
+            {posts.map((post, index) => (
+                <li key={index}>
+                    <PostCard post={post} />
+                </li>
+            ))}
+        </ul>
     )
 }
