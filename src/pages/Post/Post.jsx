18921bd73d9d4d810c95bb8e8a1eb614c9949f43
athena272 <guilhermera272@gmail.com>
2024-02-18 22:49:57 -0300

    Update Post.jsx

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,7 +1,60 @@
-import styles from './Post.module.scss'
+import './Post.css';
+import styles from './Post.module.css';
 
-import { Route, Routes, useParams } from 'react-router-dom'
-import posts from 'assets/posts'
-import PostModel from '../../component/PostModel'
-import ReactMarkdown from 'react-markdown'
-import NotFou
+import { Route, Routes, useParams } from "react-router-dom"
+import ReactMarkdown from "react-markdown";
+import posts from "assets/json/posts.json";
+import PostModel from "../../component/PostModel/PostModel";
+import NotFound from '../NotFound/NotFound';
+import PaginaPadrao from 'componentes/PaginaPadrao';
+import PostCard from 'componentes/PostCard';
+
+export default function Post() {
+    const parametros = useParams();
+
+    const post = posts.find((post) => {
+        return post.id === Number(parametros.id);
+    })
+
+    if (!post) {
+        return <NotFound />
+    }
+
+    const postsRecomendados = posts
+        .filter((post) => post.id !== Number(parametros.id))
+        .sort((a, b) => b.id - a.id)
+        .slice(0, 4);
+
+    return (
+        <Routes>
+            <Route path="*" element={<PaginaPadrao />}>
+                <Route index element={
+                    <PostModel
+                        fotoCapa={`/assets/posts/${post.id}/capa.png`}
+                        titulo={post.titulo}
+                    >
+                        <div className="post-markdown-container">
+                            <ReactMarkdown>
+                                {post.texto}
+                            </ReactMarkdown>
+                        </div>
+
+                        <h2 className={styles.tituloOutrosPosts}>
+                            Outros posts que você pode gostar:
+                        </h2>
+
+                        <ul className={styles.postsRecomendados}>
+                            {postsRecomendados.map((post) => (
+                                <li key={post.id}>
+                                    <PostCard post={post} />
+                                </li>
+                            ))}
+                        </ul>
+
+                    </PostModel>
+                } />
+            </Route>
+        </Routes>
+
+    )
+}
