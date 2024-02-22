8f38a39bb3c0ba79e979e80a530c69015f1434df
athena272 <guilhermera272@gmail.com>
2024-02-22 16:13:02 -0300

    Update NotFound.jsx

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,5 @@
 import styles from './NotFound.module.scss';
-import erro404 from 'assets/images/erro_404.png'
+import erro404 from '/assets/images/erro_404.png'
 import MainButton from '../../component/MainButton/MainButton';
 import { useNavigate } from 'react-router-dom';
 
